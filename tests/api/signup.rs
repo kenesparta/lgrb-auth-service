72c077749e282dc244b48763dea58613be303de5
Ken Esparta <kenesparta@pm.me>
2025-07-26 18:04:13 -0500

    feat: adding 409 and 400 test cases in test to handle these edge cases

--- tests/api/signup.rs
+++ tests/api/signup.rs
@@ -52,3 +52,46 @@ async fn should_return_201_if_valid_input() {
         expected_response
     );
 }
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let test_cases = [
+        serde_json::json!({
+            "email": get_random_email(),
+            "password": "password123",
+            "2FA": true
+        }),
+        serde_json::json!({
+            "email": get_random_email(),
+            "passwords": "123123",
+            "requires2FA": false
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            StatusCode::BAD_REQUEST,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_409_if_email_already_exists() {
+    let app = TestApp::new().await;
+
+    let tc = serde_json::json!({
+        "email": get_random_email(),
+        "passwords": "123123",
+        "requires2FA": false
+    });
+
+    app.post_signup(&tc).await;
+    let response = app.post_signup(&tc).await;
+    assert_eq!(response.status().as_u16(), StatusCode::CONFLICT,);
+}
