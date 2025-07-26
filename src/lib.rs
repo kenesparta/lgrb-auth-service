53ab634455b8fe68be761da74cb9cceb09904fc7
Ken Esparta <kenesparta@pm.me>
2025-07-26 18:15:03 -0500

    feat: implementing IntoResponse for Auth API

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,36 @@ use crate::routes::{health_check, login, logout, signup, verify_2fa, verify_toke
 use app_state::AppState;
 use axum::routing::{get, post};
 use axum::serve::Serve;
-use axum::Router;
+use axum::{Json, Router};
 use std::error::Error;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde::{Deserialize, Serialize};
 use tower_http::services::ServeDir;
+use crate::domain::AuthAPIError;
 
 pub struct Application {
     server: Serve<Router, Router>,
     pub address: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub error_message: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
+            AuthAPIError::UnexpectedError => (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error"),
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+        };
+        let body = Json(ErrorResponse {
+            error_message: error_message.to_string(),
+        });
+        (status, body).into_response()
+    }
 }
 
 impl Application {
