54dbc88456e2c7e0fd8f21e69d0d57ee82f82501
Ken Esparta <kenesparta@pm.me>
2025-08-03 22:52:22 -0500

    feat: Implement verify-token route
    - Adding gRPC verify token implementation
    - Creating log-out route
    - Adding tests accordingly

--- tests/api/logout.rs
+++ tests/api/logout.rs
@@ -1,9 +1,85 @@
 use crate::helpers::TestApp;
+use auth_service::utils::JWT_COOKIE_NAME;
+use fake::faker::internet::en::{Password as FakePassword, SafeEmail};
+use fake::Fake;
+use reqwest::{StatusCode, Url};
 
 #[tokio::test]
-async fn test_get_logout() {
+async fn should_return_400_if_jwt_cookie_missing() {
     let app = TestApp::new().await;
-    let response = app.get_logout().await;
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), StatusCode::BAD_REQUEST);
+}
 
-    assert_eq!(response.status(), reqwest::StatusCode::OK);
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+
+    app.cookie_jar.add_cookie_str(
+        &format!(
+            "{}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
+            JWT_COOKIE_NAME
+        ),
+        &Url::parse("http://127.0.0.1").expect("Failed to parse URL"),
+    );
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), StatusCode::UNAUTHORIZED);
+}
+
+#[tokio::test]
+async fn should_return_200_if_valid_jwt_cookie() {
+    let app = TestApp::new().await;
+    let fake_email: String = SafeEmail().fake();
+    let fake_password: String = FakePassword(8..20).fake();
+
+    let response = app
+        .post_signup(&serde_json::json!({
+            "email": fake_email.clone(),
+            "password": fake_password,
+            "requires2FA": false
+        }))
+        .await;
+    assert_eq!(response.status().as_u16(), StatusCode::CREATED);
+
+    let response = app
+        .post_login(&serde_json::json!({
+            "email": fake_email,
+            "password": fake_password,
+        }))
+        .await;
+    assert_eq!(response.status().as_u16(), StatusCode::OK);
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn should_return_400_if_logout_called_twice_in_a_row() {
+    let app = TestApp::new().await;
+    let fake_email: String = SafeEmail().fake();
+    let fake_password: String = FakePassword(8..20).fake();
+
+    let response = app
+        .post_signup(&serde_json::json!({
+            "email": fake_email.clone(),
+            "password": fake_password,
+            "requires2FA": false
+        }))
+        .await;
+    assert_eq!(response.status().as_u16(), StatusCode::CREATED);
+
+    let response = app
+        .post_login(&serde_json::json!({
+            "email": fake_email,
+            "password": fake_password,
+        }))
+        .await;
+    assert_eq!(response.status().as_u16(), StatusCode::OK);
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), StatusCode::OK);
+
+    let response = app.post_logout().await;
+    assert_eq!(response.status().as_u16(), StatusCode::BAD_REQUEST);
 }
